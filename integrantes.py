e5d992467f55378fb70fc940f865a0387174ee31
OpazoCris <cr.opazoa@duocuc.cl>
2025-07-05 22:57:07 +0000

    Se agregan los scripts de parte 1 ET

---
+++ integrantes.py
@@ -0,0 +1,12 @@
+def mostrar_integrantes():
+    integrantes = [
+        "Cristopher Opazo",
+        "Felipe Sobarzo"
+    ]
+    print("Integrantes del grupo: ")
+    for nombre in integrantes:
+        print("-", nombre)
+
+
+if __name__ == "__main__":
+    mostrar_integrantes()
---
+++ verificador_de_AS.py
@@ -0,0 +1,16 @@
+def es_as_privado(asn):
+    return (64512 <= asn <= 65534) 
+
+def main():
+    print("===== Verificador de ASN BGP =====")
+    try:
+        asn = int(input("Ingrese un numero de Sistema Autonomo (AS) PARA BGP: "))
+        if es_as_privado(asn):
+            print(f"\nEl ASN {asn} corresponde a un AS PRIVADO.")
+        else:
+            print(f"\nEl ASN {asn} corresponde a un AS PÚBLICO.")
+    except ValueError:
+        print("\nError: debe ingresar un número entero válido.")
+
+if __name__ == "__main__":
+    main()
